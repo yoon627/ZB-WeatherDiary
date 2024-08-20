17373beb2ab813a901e9fe3c71ab2250b037dd4e
yoon627 <science627@naver.com>
2024-08-20 15:54:58 +0900

    test: Jdbc, Jpa 테스트 추가

---
+++ src/test/java/zerobase/weatherdiary/repository/JdbcMemoRepositoryTest.java
@@ -0,0 +1,43 @@
+package zerobase.weatherdiary.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import zerobase.weatherdiary.domain.Memo;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+@Transactional
+class JdbcMemoRepositoryTest {
+
+    @Autowired
+    JdbcMemoRepository jdbcMemoRepository;
+
+    @Test
+    void insertMemoTest() {
+        //given
+        Memo newMemo = new Memo(1, "insert memo test");
+
+        //when
+        jdbcMemoRepository.save(newMemo);
+
+        //then
+        Optional<Memo> result = jdbcMemoRepository.findById(1);
+        assertEquals(result.get().getText(), "insert memo test");
+    }
+
+    @Test
+    void findAllMemosTest() {
+        //given
+        //when
+        List<Memo> memoList = jdbcMemoRepository.findAll();
+        //then
+        assertNotNull(memoList);
+    }
+}
