6a79d54afd4dc17f9645434f9eacad82fb0ff02a
yoon627 <science627@naver.com>
2024-08-20 13:33:44 +0900

    Initial commit

---
+++ src/main/java/zerobase/weatherdiary/WeatherDiaryApplication.java
@@ -0,0 +1,13 @@
+package zerobase.weatherdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherDiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherDiaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/zerobase/weatherdiary/WeatherDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package zerobase.weatherdiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherDiaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
