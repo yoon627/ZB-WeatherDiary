29aca7e21f0689ae3f87c0f09fa03dbc513d33cb
yoon627 <science627@naver.com>
2024-08-23 18:14:03 +0900

    feat: API document를 생성해주는 Swagger 추가

---
+++ src/main/java/zerobase/weatherdiary/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package zerobase.weatherdiary.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("날씨 일기 프로젝트")
+                .description("날씨 일기를 CRUD 할 수 있는 백엔드 API 입니다.")
+                .version("1.0.0");
+    }
+}
--- src/main/java/zerobase/weatherdiary/controller/DiaryController.java
+++ src/main/java/zerobase/weatherdiary/controller/DiaryController.java
@@ -1,5 +1,7 @@
 package zerobase.weatherdiary.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 import zerobase.weatherdiary.domain.Diary;
@@ -17,36 +19,41 @@ public class DiaryController {
         this.diaryService = diaryService;
     }
 
+    @Operation(summary = "일기 텍스트와 날씨를 이용해 DB에 일기 저장", description = "날씨 일기를 생성합니다.")
     @PostMapping("/create/diary")
     void createDiary(
-            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date,
-            @RequestBody String text) {
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @Parameter(description = "생성 날짜", example = "2024-08-23") LocalDate date,
+            @RequestBody @Parameter(description = "일기 내용", example = "안녕하세요!") String text) {
         diaryService.createDiary(date, text);
     }
 
+    @Operation(summary = "선택한 날짜의 모든 일기 데이터 조회", description = "날씨 일기를 조회합니다.")
     @GetMapping("/read/diary")
     List<Diary> readDiary(
-            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @Parameter(description = "조회 날짜", example = "2024-08-23") LocalDate date) {
         return diaryService.readDiary(date);
     }
 
+    @Operation(summary = "선택한 기간동안 모든 일기 데이터 조회", description = "기간동안 날씨 일기를 조회합니다.")
     @GetMapping("/read/diaries")
     List<Diary> readDiaries(
-            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
-            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate) {
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @Parameter(description = "시작 날짜", example = "2024-08-23") LocalDate startDate,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @Parameter(description = "끝 날짜", example = "2024-08-23") LocalDate endDate) {
         return diaryService.readDiaries(startDate, endDate);
     }
 
+    @Operation(summary = "선택한 날의 첫번째 일기 수정", description = "날씨 일기를 수정합니다.")
     @PutMapping("/update/diary")
     void updateDiary(
-            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @Parameter(description = "수정 날짜", example = "2024-08-23") LocalDate date,
             @RequestBody String text) {
         diaryService.updateDiary(date, text);
     }
 
+    @Operation(summary = "선택한 날의 일기들 삭제", description = "날씨 일기를 삭제합니다.")
     @DeleteMapping("/delete/diary")
     void deleteDiary(
-            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @Parameter(description = "삭제 날짜", example = "2024-08-23") LocalDate date) {
         diaryService.deleteDiary(date);
     }
 }
