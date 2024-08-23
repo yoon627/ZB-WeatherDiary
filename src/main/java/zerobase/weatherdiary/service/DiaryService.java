7917a0647afd0089664a423ad2ffb351a25c31f2
yoon627 <science627@naver.com>
2024-08-23 12:34:23 +0900

    feat: logback을 이용한 log 저장 추가

--- src/main/java/zerobase/weatherdiary/service/DiaryService.java
+++ src/main/java/zerobase/weatherdiary/service/DiaryService.java
@@ -4,11 +4,13 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
+import zerobase.weatherdiary.WeatherDiaryApplication;
 import zerobase.weatherdiary.domain.DateWeather;
 import zerobase.weatherdiary.domain.Diary;
 import zerobase.weatherdiary.repository.DateWeatherRepository;
@@ -34,6 +36,8 @@ public class DiaryService {
 
     private final DateWeatherRepository dateWeatherRepository;
 
+    private static final Logger logger = LoggerFactory.getLogger(WeatherDiaryApplication.class);
+
     public DiaryService(DiaryRepository diaryRepository, DateWeatherRepository dateWeatherRepository) {
         this.diaryRepository = diaryRepository;
         this.dateWeatherRepository = dateWeatherRepository;
@@ -43,10 +47,12 @@ public class DiaryService {
     @Scheduled(cron = "0 0 1 * * *")
     public void saveWeatherDate() {
         dateWeatherRepository.save(getWeatherFromApi());
+        logger.info("Saving weather success");
     }
 
     @Transactional(readOnly = false)
     public void createDiary(LocalDate date, String text) {
+        logger.info("started to create diary");
         //날씨 데이터 가져오기 (API 에서 가져오기 or DB 에서 가져오기)
         DateWeather dateWeather = getDateWeather(date);
 
@@ -57,6 +63,7 @@ public class DiaryService {
         nowDiary.setDate(date);
 
         diaryRepository.save(nowDiary);
+        logger.info("end to create diary");
     }
 
     private DateWeather getWeatherFromApi() {
@@ -85,6 +92,7 @@ public class DiaryService {
     }
 
     public List<Diary> readDiary(LocalDate date) {
+        logger.debug("read diary");
         return diaryRepository.findAllByDate(date);
     }
 
